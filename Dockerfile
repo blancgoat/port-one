952c24d9d33cf8cbaf36d10f44fb68254b9f7261
blancgoat <blancgoat@gmail.com>
2024-11-29 23:48:31 +0900

    setup ci4

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM php:8.3.14-apache
-
-RUN pecl install xdebug-3.4.0
-RUN docker-php-ext-enable xdebug
--- src/index.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html lang="ko">
-<body>
-    <div class="container">
-        <?php include 'project1/project1.php'; ?>
-    </div>
-</body>
-</html>
